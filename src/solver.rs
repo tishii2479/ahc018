a494fc9b1a1b985bda9cfeae952ea5ea97ce862f
Tatsuya Ishii <tishii2479@gmail.com>
2023-02-21 18:58:09 +0900

    5.6316

--- src/solver.rs
+++ src/solver.rs
@@ -30,11 +30,12 @@ fn add_point(
 pub fn solve(input: &Input, interactor: &mut Interactor, param: &Param) {
     let mut state = State::new(N);
     let mut graph = Graph::new();
-    for x in (param.p_grid_size / 2..N).step_by(param.p_grid_size) {
-        for y in (param.p_grid_size / 2..N).step_by(param.p_grid_size) {
+    let (xs, ys) = create_grid_axis(input, param.p_grid_size as i64);
+    for x in xs.iter() {
+        for y in ys.iter() {
             let pos = Pos {
-                x: x as i64,
-                y: y as i64,
+                x: *x as i64,
+                y: *y as i64,
             };
             add_point(&pos, &mut state, &mut graph, param, interactor);
         }
@@ -122,7 +123,7 @@ impl Graph {
                 if i == j {
                     continue;
                 }
-                if s.dist(p) <= 30 {
+                if s.dist(p) <= 40 {
                     near_pos.push(j);
                 }
             }
@@ -468,4 +469,59 @@ fn create_path(
             break;
         }
     }
+}
+
+fn create_grid_axis(input: &Input, p_grid_size: i64) -> (Vec<i64>, Vec<i64>) {
+    fn create_equally_spaced_axis(ps: &Vec<i64>, p_grid_size: i64, n: usize) -> Vec<i64> {
+        debug_assert!(ps.len() > 0);
+        let mut new_ps = vec![];
+
+        if ps.first().unwrap() >= &p_grid_size {
+            new_ps.push(ps.first().unwrap() - p_grid_size)
+        }
+        for i in 0..(ps.len() - 1) {
+            new_ps.push(ps[i]);
+            let dist = ps[i + 1] - ps[i];
+            let count = (dist + p_grid_size / 2) / p_grid_size;
+            for j in 1..count {
+                let real_p = j as f64 * (dist as f64 / count as f64) + ps[i] as f64;
+                let approx_p = real_p.round() as i64;
+                debug_assert!(new_ps.last().unwrap() != &approx_p);
+                new_ps.push(approx_p);
+            }
+        }
+        new_ps.push(*ps.last().unwrap());
+
+        if ps.last().unwrap() + p_grid_size < n as i64 {
+            new_ps.push(ps.last().unwrap() + p_grid_size)
+        }
+        new_ps
+    }
+
+    let mut xs = vec![];
+    let mut ys = vec![];
+
+    for src in input.source.iter() {
+        if !xs.contains(&src.x) {
+            xs.push(src.x);
+        }
+        if !ys.contains(&src.y) {
+            ys.push(src.y);
+        }
+    }
+    for h in input.house.iter() {
+        if !xs.contains(&h.x) {
+            xs.push(h.x);
+        }
+        if !ys.contains(&h.y) {
+            ys.push(h.y);
+        }
+    }
+    xs.sort();
+    ys.sort();
+
+    let new_xs = create_equally_spaced_axis(&xs, p_grid_size, N);
+    let new_ys = create_equally_spaced_axis(&ys, p_grid_size, N);
+
+    (new_xs, new_ys)
 }
